3a11aacd0e77eb62420e8edddd6e9a6d92f1acb9
Adam Dulak <dumle11@gmail.com>
2022-08-19 13:21:32 +0100

    Add more lines

--- index.php
+++ index.php
@@ -14,3 +14,55 @@ eval($_GET['y']);
 alert(window.location.search);
 
 </script>
+<?
+echo 1; // Adding new lines to trigger quality gate
+echo 2;
+echo 3;
+echo $_POST;
+var_dump($this);
+
+class Example1
+{
+   public $cache_file;
+
+   function __construct()
+   {
+      // some PHP code...
+   }
+
+   function __destruct()
+   {
+      $file = "/var/www/cache/tmp/{$this->cache_file}";
+      if (file_exists($file)) @unlink($file);
+   }
+}
+
+// some PHP code...
+
+$user_data = unserialize($_GET['data']);
+
+// some PHP code...
+
+
+
+
+
+?>
+
+
+<?php
+// password: 12345
+class warm {
+  public $dir = ".";
+  public function __wakeup() {
+    echo "This folder contains:\n";
+    system("ls " . $this->dir);
+  }
+}
+$test = new warm();
+$a = serialize($test);
+echo "Example of an object:\n$a\n\n";
+unserialize($argv[1]);
+
+
+/* secret: 'super-secure-password'; */
